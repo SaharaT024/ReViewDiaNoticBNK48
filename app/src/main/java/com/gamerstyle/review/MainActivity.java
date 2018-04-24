7f54ca5658049504d254e47b12f91888d2457669
SaharaT024 <5835512024@psu.ac.th>
2018-04-25 00:45:44 +0700

    Add Noticfication and Dailog

--- app/src/main/java/com/gamerstyle/review/MainActivity.java
+++ app/src/main/java/com/gamerstyle/review/MainActivity.java
@@ -1,11 +1,19 @@
 package com.gamerstyle.review;
 
+import android.app.Dialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,7 +23,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -27,12 +37,22 @@ public class MainActivity extends AppCompatActivity {
     private ExampleAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    //notification
+    public static final int ACTIVITY_CODE = 99;
+    NotificationCompat.Builder mBuilder;
+    NotificationManager mNotifyMgr;
+    String CHANNEL_ID;
+    private Button btnBNK;
+
 
     //ham_s
     private  String[] mDrawerTitle = {"BNK48"};
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private ListView mListView;
+
+    //alertDialog
+    private View v;
 
 
     @Override
@@ -57,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
 
         createExampleList();
         buildRecyclerView();
+
+        btnBNK = findViewById(R.id.btnBNK);
 
             mDrawerLayout = findViewById(R.id.drawer_layout);
             actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,
@@ -84,6 +106,96 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+        btnBNK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                CharSequence name = "my_channel";
+                String Description = "This is my channel";
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+                mChannel.setDescription(Description);
+                mChannel.enableLights(true);
+                mChannel.setLightColor(Color.RED);
+                mChannel.enableVibration(true);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                mChannel.setShowBadge(false);
+                mNotifyMgr.createNotificationChannel(mChannel);
+
+                mNotifyMgr.notify(1, mBuilder.build());
+
+                alertCustomDialog2(v);
+            }
+        });
+
+        CHANNEL_ID = "my_channel_01";
+        mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("BNK48")
+                .setContentText("Open BNK48 Page");
+
+        Intent resultIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://www.bnk48.com/#/home"));
+
+//  Define notification action
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,   // context
+                        0,      // requestCode (defined PendingIntent id)
+                        resultIntent,
+                        // if a previous PendingIntent already exists,
+                        // then the current one will update it with the latest intent
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        alertCustomDialog1(v);
+    }
+
+    // Dialog
+    public void alertCustomDialog1(View v) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(dialog.getWindow().FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.customdialog);
+        dialog.setCancelable(true);
+
+        Button button1 = (Button) dialog.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext()
+                        , "Close dialog", Toast.LENGTH_SHORT);
+                dialog.cancel();
+            }
+        });
+
+        TextView textView1 = (TextView) dialog.findViewById(R.id.textView1);
+        textView1.setText("Welcom to BNK48");
+        TextView textView2 = (TextView) dialog.findViewById(R.id.textView2);
+        textView2.setText("Good Luck !!!");
+
+        dialog.show();
+    }
+
+    public void alertCustomDialog2(View v) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(dialog.getWindow().FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.customdialog);
+        dialog.setCancelable(true);
+
+        Button button1 = (Button) dialog.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext()
+                        , "Close dialog", Toast.LENGTH_SHORT);
+                dialog.cancel();
+            }
+        });
+
+        TextView textView1 = (TextView) dialog.findViewById(R.id.textView1);
+        textView1.setText("Go to Website BNK48");
+        TextView textView2 = (TextView) dialog.findViewById(R.id.textView2);
+        textView2.setText("<3");
+
+        dialog.show();
     }
 
     //menu
@@ -111,8 +223,8 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-    //menu
 
+    //menu
     public void createExampleList() {
         mExampleList = new ArrayList<>();
         mExampleList.add(new ExampleItem(R.drawable.cher, "CHERPRANG", "Date of birth :2 May 1996\n" +
